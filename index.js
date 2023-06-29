1387ae057d7af2f7746e6df7955359a75beccdef
Koloina <koloinaimaginieur@gmail.com>
2023-06-29 15:27:22 +0300

    improve

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 require('dotenv').config();
+const { instrument } = require("@socket.io/admin-ui");
 
 const io = require("socket.io")(process.env.PORT, {
-    cors: {
-      origin: process.env.CLIENT_URL,
-      credentials: true,
-    },
-  });
-  
-  io.on("connection", (socket) => {
-    socket.on("start", (uid) => {
-      socket.join(uid);
-      console.log("Connected to socket.io "+uid);
-    });
-  
-    socket.on("notification", (to)=>{
-      console.log(to)
-      socket.in(to).emit('new notification');
-    })
+  cors: {
+    origin: ["https://admin.socket.io", process.env.CLIENT_URL],
+    credentials: true,
+  },
+});
 
-    // socket.on("typing", (room) => socket.in(room).emit("typing"));
-    // socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-  
-    socket.on("message sent", (newMessageRecieved,to) => {
-       socket.in(to).emit("new message", newMessageRecieved);
-    });
-  
-    socket.off("setup", () => {
-      console.log("USER DISCONNECTED");
-      socket.leave(uid);
-    });
+io.on("connection", (socket) => {
+  socket.on("start", (uid) => {
+    socket.join(uid);
+    console.log("Connected to socket.io " + uid);
   });
+
+  socket.on("notification", (to) => {
+    socket.in(to).emit("new notification");
+  });
+
+  // socket.on("typing", (room) => socket.in(room).emit("typing"));
+  // socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("message sent", (newMessageRecieved, to) => {
+    socket.in(to).emit("new message", newMessageRecieved);
+  });
+
+  socket.on("delete message", ({ userId, messageId, conversationId }) => {
+    socket
+      .in(userId)
+      .emit("deleted message", { messageId, conversationId, userId });
+  });
+});
+
+instrument(io, {
+  auth: false,
+});
