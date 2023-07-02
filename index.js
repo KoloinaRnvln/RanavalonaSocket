f74cc7bb64786a3fcc03f21ef79ad815ae065105
Koloina <koloinaimaginieur@gmail.com>
2023-07-02 22:06:27 +0300

    improve

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ io.on("connection", (socket) => {
     socket.in(to).emit("new notification");
   });
 
+  socket.on("relation change", ({ body, category }) => {
+    socket.in(body.to).emit("relation update", { from: body.from, category });
+  });
+
   // socket.on("typing", (room) => socket.in(room).emit("typing"));
   // socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
